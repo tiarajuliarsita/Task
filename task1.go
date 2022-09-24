9eb56e856e9d034bde6d6f9c07cbd3c48309a210
tiarajuliarsita <tiarajuliarsita@gmail.com>
2022-09-24 15:22:42 +0800

    update task2

--- task1.go
+++ task1.go
@@ -1,54 +1,54 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-)
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"log"
+// 	"net/http"
+// )
 
-type Pokemon struct {
-	Id              int    `json:"id"`
-	Name            string `json:"name"`
-	BaseExpereience int    `json:"base_experience"`
-	Height          int    `json:"height"`
-	IsDefault       bool   `json:"is_default"`
-	Order           int    `json:"order"`
-	Weight          int    `json:"weight"`
-}
+// type Pokemon struct {
+// 	Id              int    `json:"id"`
+// 	Name            string `json:"name"`
+// 	BaseExpereience int    `json:"base_experience"`
+// 	Height          int    `json:"height"`
+// 	IsDefault       bool   `json:"is_default"`
+// 	Order           int    `json:"order"`
+// 	Weight          int    `json:"weight"`
+// }
 
-func main() {
-	http.HandleFunc("/pokemon", func(w http.ResponseWriter, r *http.Request) {
-		id := r.URL.Query().Get("id")
-		resp, err := http.Get("https://pokeapi.co/api/v2/pokemon/" + id)
-		if err != nil {
-			log.Fatal("kamu gagal", err)
-		}
-		defer resp.Body.Close()
+// func main() {
+// 	http.HandleFunc("/pokemon", func(w http.ResponseWriter, r *http.Request) {
+// 		id := r.URL.Query().Get("id")
+// 		resp, err := http.Get("https://pokeapi.co/api/v2/pokemon/" + id)
+// 		if err != nil {
+// 			log.Fatal("kamu gagal", err)
+// 		}
+// 		defer resp.Body.Close()
 
-		pokemon := Pokemon{}
-		respByte, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal("wah ada eror ges", err)
-		}
+// 		pokemon := Pokemon{}
+// 		respByte, err := ioutil.ReadAll(resp.Body)
+// 		if err != nil {
+// 			log.Fatal("wah ada eror ges", err)
+// 		}
 
-		err = json.Unmarshal(respByte, &pokemon)
-		if err != nil {
-			log.Fatal("erorr again", err)
-		}
+// 		err = json.Unmarshal(respByte, &pokemon)
+// 		if err != nil {
+// 			log.Fatal("erorr again", err)
+// 		}
 
-		fmt.Println(pokemon)
-		pokemonbyte, err := json.Marshal(pokemon)
-		if err != nil {
-			log.Fatal("erorr again", err)
-		}
-		w.Header().Add("Content-Type", "application/json")
-		w.Write(pokemonbyte)
-	})
+// 		fmt.Println(pokemon)
+// 		pokemonbyte, err := json.Marshal(pokemon)
+// 		if err != nil {
+// 			log.Fatal("erorr again", err)
+// 		}
+// 		w.Header().Add("Content-Type", "application/json")
+// 		w.Write(pokemonbyte)
+// 	})
 
-	if err := http.ListenAndServe(":5000", nil); err != nil {
-		log.Fatal("erorr again", err)
-	}
+// 	if err := http.ListenAndServe(":5000", nil); err != nil {
+// 		log.Fatal("erorr again", err)
+// 	}
 
-}
+// }
--- task2.go
+++ task2.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	// "strconv"
+)
+
 type Note struct {
 	Id    int    `json:"id"`
 	Title string `json:"title"`
@@ -10,87 +18,69 @@ type Request struct {
 	Body  string `json:"body"`
 }
 type Response struct {
-	Code    int    `json:"id"`
+	Code    int    `json:"code"`
 	Status  string `json:"status"`
 	Message string `json:"message"`
 }
-
 
 var (
 	notes []Note
 )
 
-func sendResponse(w http.ResponseWriter, code int, message string, status string{}) {
+// respon
+func sendResponse(w http.ResponseWriter, code int, status string, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	var response Response
 	response.Code = code
 	response.Status = status
-	response.Message = message	
+	response.Message = message
 
+	dByte, _ := json.Marshal(response)
 
-	noteByte, err := json.Marshal(response)
-	if err != nil {
-		log.Fatal("ada error", err)
-	}
-
-	w.Write(noteByte)
+	w.Write(dByte)
 }
 
-
-func main(){
-	http.HandleFunc("/notes", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			defer r.Body.Close()
-			dByte, err := io.ReadAll(r.Body)
-			if err != nil {
-				sendResponse(w, http.StatusInternalServerError, "Terjadi Masalah", nil)
-				return
-			
-
-			var note Note
-			index := len(notes)
-			json.Unmarshal(dByte, &note)
-
-			note.Index = index
-
-			notes = append(notes, note)
-			sendResponse(w, http.CodeCreated, 200, "")
-			sendResponse(w, http.StatusCreated, "succes", "")
-			sendResponse(w, http.MessageCreated, "Note baru Berhasil diTambahkan", "")
-			return
-		}
-		// if r.Method == http.MethodDelete {
-		// 	id := r.URL.Query().Get("id")
-
+// GetNote
+func NOTES(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		// id := r.URL.Query().Get("id")
+		// if id != "" {
 		// 	idT, _ := strconv.Atoi(id)
-		// 	remove(notes, idT)
-		// 	sendResponse(w, http.StatusOK, "Data Berhasil dihapus", "")
-		// 	return
-
+		// 	notes[idT]
 		// }
+		DNote, err := json.Marshal(notes)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(DNote)
+		return
 	}
 
+	if r.Method == "POST" {
+		var nt Note
+		decodeJSON := json.NewDecoder(r.Body)
+		if err := decodeJSON.Decode(&nt); err != nil {
+			log.Fatal(err)
+		}
+		nt.Id = len(notes) + 1
+		notes = append(notes, nt)
 
+		DNote, _ := json.Marshal(nt) // ke byte
+		w.Write(DNote)
+		return
+	}
 
+	http.Error(w, "hayo  capek ya wkwkw?", http.StatusNotFound)
+}
 
+func main() {
+	http.HandleFunc("/notes", NOTES)
+	fmt.Println("on proses")
+	if err := http.ListenAndServe(":7000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
 
-
-
-
-
-// func TambahData(){
-
-// }
-
-// func UbahData(){
-
-// }
-
-// func HapusData(){
-
-// }
-
-// }
